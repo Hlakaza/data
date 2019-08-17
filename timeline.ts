988f5f3be1d088c3dc00e270e00ab7172f08bc75
Kgaogelothorn <kgaogelothorn@gmail.com>
2019-08-17 14:26:41 +0200

    refactor(timeline):added timeline data

---
+++ timeline.ts
@@ -0,0 +1,61 @@
+let timeline: [
+  {
+    year: "1998";
+    para1: `The Bidvest Group acquires 100% of The Rennies Group, which includes the Rennies Foreign Exchange business.`;
+    para2: "";
+  },
+
+  {
+    year: "2000";
+    para1: `The Bidvest Group is granted a banking license as Rennies Bank, incLuding the Rennies Foreign Exchange business.`;
+    para2: "Rennies Foreign Exchange Launches International money transfer via MoneyGram. ";
+  },
+
+  {
+    year: "2007";
+    para1: `The Bidvest Group leverages the equity of the Bidvest brand and rebrands Rennies Bank to Bidvest Bank.`;
+    para2: "Focused drive to attract and retain talent with banking expertise.";
+  },
+
+  {
+    year: "2010";
+    para1: `Credit and lending expertise join the bank. `;
+    para2: "";
+  },
+
+  {
+    year: "2009 ";
+    para1: `Savings and Investment accounts launched to markert.`;
+    para2: "Bidvest acquires McCarthy Fleet Solutions from McCarthy group and offers fleet finance and Management Services.";
+  },
+
+  {
+    year: "2008";
+    para1: `Flagship Foreign Exchange product, World Currency Card launched.`;
+    para2: "Global Payment Online launched, International Trading platform launched.";
+  },
+
+  {
+    year: "2011";
+    para1: ` Personal transactional banking launched. `;
+    para2: "";
+  },
+
+  {
+    year: "2015";
+    para1: `Business transactional banking launched. `;
+    para2: "";
+  },
+
+  {
+    year: "2016";
+    para1: `Business philosophy 'Let's get it done' communicated to the market. `;
+    para2: "";
+  },
+
+  {
+    year: "2017";
+    para1: `Bidvest Bank launches business merchant services. `;
+    para2: "";
+  }
+];
