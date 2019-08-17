d77b737a86983315fbdb5f071f6051c5418de02d
Kgaogelothorn <kgaogelothorn@gmail.com>
2019-08-17 21:03:44 +0200

    refactor(contact_us):added contactUs data

--- contact_us.ts
+++ contact_us.ts
@@ -10,21 +10,21 @@
       heading: 'JHB corporate head JHB corporate head office postal address',
       subheading: '',
       template: '',
-      content: `1 Park Lane <br/> Wierda Valley <br/> Sanddton 2196 <br/> south Africa`,
+      content: `P O Box 185 <br/>Johannesburg <br/>2000 <br/> South Africa`,
       imageSrc: 'arrow-down'
     },
     {
       heading: 'KZN corporate office',
       subheading: '',
       template: '',
-      content: `1 Park Lane <br/> Wierda Valley <br/> Sanddton 2196 <br/> south Africa`,
+      content: `2nd Floor, Lincoln on the Lake <br/> 2 The High Street <br/> Parkside <br/> Umhlanga Ridge <br/> 4319 <br/> South Africa`,
       imageSrc: 'arrow-down'
     },
     {
       heading: 'Western Cape corporate office',
       subheading: '',
       template: '',
-      content: `1 Park Lane <br/> Wierda Valley <br/> Sanddton 2196 <br/> south Africa`,
+      content: `6th Floor <br/> 33 Martin Hammerschlag way <br/> Foreshore <br/> Cape Town <br/> South Africa`,
       imageSrc: 'arrow-down'
     },
 ];
@@ -32,8 +32,8 @@
 export const contact = {
 heading: 'Contact us',
 enquries: [
-    'Contact Request',
-    'Example request',
-    'Change request'
+    'General query/Comment',
+    'Compliment',
+    'Complain'
 ]
 };
