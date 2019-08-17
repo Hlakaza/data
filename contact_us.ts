ff35e40c2493f189a70640dc8b167e87f271d8c2
hlakaza <nhlalucky9@gmail.com>
2019-08-17 14:54:19 +0200

    refactor(contact): added contact file

---
+++ contact_us.ts
@@ -0,0 +1,39 @@
+ export const accordions =  [
+    {
+      heading: 'JHB corporate head office',
+      subheading: '',
+      template: '',
+      content: `1 Park Lane <br/> Wierda Valley <br/> Sanddton 2196 <br/> south Africa`,
+      imageSrc: 'arrow-down'
+    },
+    {
+      heading: 'JHB corporate head JHB corporate head office postal address',
+      subheading: '',
+      template: '',
+      content: `1 Park Lane <br/> Wierda Valley <br/> Sanddton 2196 <br/> south Africa`,
+      imageSrc: 'arrow-down'
+    },
+    {
+      heading: 'KZN corporate office',
+      subheading: '',
+      template: '',
+      content: `1 Park Lane <br/> Wierda Valley <br/> Sanddton 2196 <br/> south Africa`,
+      imageSrc: 'arrow-down'
+    },
+    {
+      heading: 'Western Cape corporate office',
+      subheading: '',
+      template: '',
+      content: `1 Park Lane <br/> Wierda Valley <br/> Sanddton 2196 <br/> south Africa`,
+      imageSrc: 'arrow-down'
+    },
+];
+
+export const contact = {
+heading: 'Contact us',
+enquries: [
+    'Contact Request',
+    'Example request',
+    'Change request'
+]
+};
