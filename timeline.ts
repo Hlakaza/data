b80c8e736aecc804a1d4aff684236c684f03fb63
hlakaza <nhlalucky9@gmail.com>
2019-08-17 13:03:33 +0200

    refactor(timeline): added time line files

---
+++ timeline.ts
@@ -0,0 +1,13 @@
+let timeline: [
+       {
+         year: '1998',
+         para1: `The Bidvest Group acquires 100% of The Rennies Group, which includes the Rennies Foreign Exchange business.`,
+         para2: ''
+       },
+       {
+         year: '2000',
+         para1: `The Bidvest Group is granted a banking licence as Rennies Bank, including the Rennies Foreign Exchange business.`,
+         para2: 'Rennies Foreign Exchange launches international money transfers via MoneyGram.'
+       }
+
+]
